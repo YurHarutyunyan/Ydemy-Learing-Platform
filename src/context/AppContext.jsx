e2906d4b0348be1bfc9ff887fcb14ff0c22474a5
yuriharutyunyan <yurih@adobe.com>
2025-10-05 17:16:21 +0400

    course cards added, course card creation, dummy data displaying

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,7 +1,26 @@
+import React, {useState,useEffect,useCallback} from "react";
 import { AppContext } from "./createAppContext";
+import { dummyCourses } from "../assets/assets";
 
 export const AppContextProvider = (props)=>{
-    const value = {}
+    
+    const currency = import.meta.env.VITE_CURRENCY;
+    
+    const [allCourses,setAllCourses] = useState([]);
+
+    //Fetch all courses
+    const fetchAllCourses = useCallback(()=>{
+        setAllCourses(dummyCourses)
+    },[])
+
+    const value = {
+        currency,allCourses
+    }
+
+    useEffect( ()=>{
+         fetchAllCourses()
+    },[fetchAllCourses])
+
     return (
         <AppContext.Provider value = {value}>
             {props.children}
