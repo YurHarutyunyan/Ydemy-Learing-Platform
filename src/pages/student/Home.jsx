d76b0cb675ff0d6016b5ef8fec25037e07675108
yuriharutyunyan <yurih@adobe.com>
2025-10-05 18:44:27 +0400

    testimonial section

--- src/pages/student/Home.jsx
+++ src/pages/student/Home.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Hero from '../../components/student/Hero'
 import Companies from '../../components/student/Companies'
 import CoursesSection from '../../components/student/CoursesSection'
-
+import TestimonialSection from '../../components/student/TestimonialSection'
 function Home() {
   return (
     <div className='flex flex-col items-center space-y-7 text-center'>
       <Hero/>
       <Companies/>
       <CoursesSection/>
+      <TestimonialSection/>
     </div>
   )
 }
