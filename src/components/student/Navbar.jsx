2d93fd641778c96fa680ee72c8863f937a5dfd79
yuriharutyunyan <yurih@adobe.com>
2025-10-05 17:43:54 +0400

    educator page

--- src/components/student/Navbar.jsx
+++ src/components/student/Navbar.jsx
@@ -1,23 +1,26 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import assets from '../../assets/assets'
 import { Link } from 'react-router-dom';
 import { useClerk,UserButton, useUser } from '@clerk/clerk-react';
+import { AppContext } from '../../context/createAppContext';
 
 export default function Navbar() {
 
   const isCourseListPage = location.pathname.includes('/course-list');
 
+  const {navigate,isEducator,setIsEducator} = useContext(AppContext)
+
   const {openSignIn} = useClerk();
   const {user} = useUser()
   return (
     <div className={`flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${isCourseListPage ? 'b-white' : 'bg-cyan-100/70'}`}>
-        <img src={assets.logo} alt='logo' className='w-28 lg:w-32 cursor-pointer'/>
+        <img src={assets.logo} alt='logo' onClick={()=>navigate('/')}className='w-28 lg:w-32 cursor-pointer'/>
         <div className='hidden md:flex items-center gap-5 text-gray-500'>
             <div className='flex items-center gap-5'>
                 {
                   user &&
                   <>
-                    <button className=''>Become educator</button> | <Link to='my-enrollments'>My Enrollments</Link>
+                    <button className='' onClick={()=>{navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become educator'}</button> | <Link to='my-enrollments'>My Enrollments</Link>
                   </>
                 }
             </div>
@@ -34,7 +37,7 @@ export default function Navbar() {
             {
               user &&
               <>
-                <button className=''>Become educator</button> 
+                <button className=''onClick={() => {navigate('/educator')}}>{isEducator ? 'Educator Dashboard' : 'Become educator'}</button> 
                 |  <Link to='my-enrollments'>My Enrollments</Link>
               </>
             }
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,22 +1,37 @@
 import React, {useState,useEffect,useCallback} from "react";
+import { useNavigate } from "react-router-dom";
 import { AppContext } from "./createAppContext";
 import { dummyCourses } from "../assets/assets";
 
 export const AppContextProvider = (props)=>{
     
     const currency = import.meta.env.VITE_CURRENCY;
+
+    const navigate = useNavigate()
     
     const [allCourses,setAllCourses] = useState([]);
+    const [isEducator,setIsEducator] = useState(true);
 
     //Fetch all courses
     const fetchAllCourses = useCallback(()=>{
         setAllCourses(dummyCourses)
     },[])
 
-    const value = {
-        currency,allCourses
+    
+    //function to calculate average rating of course
+    const calculateRating = (course)=>{
+        if(course.courseRatings.length === 0){
+            return 0;
+        }
+        let totalRating = 0;
+        course.courseRatings.forEach((rating)=>{
+            totalRating += rating.rating;
+        })
+        return totalRating / course.courseRatings.length;
     }
-
+    const value = {
+        currency,allCourses,navigate,calculateRating,isEducator,setIsEducator
+    }
     useEffect( ()=>{
          fetchAllCourses()
     },[fetchAllCourses])
